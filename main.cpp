1dc4abe84a96a1f552425d4d9a7fb8bf35fde104
Aleksey Cheusov <vle@gmx.net>
2005-07-29 14:39:42 +0000

    new features

--- main.cpp
+++ main.cpp
@@ -2,17 +2,30 @@
 #include <string>
 #include <string.h>
 #include <map>
-//#include <hash_map>
+
+//#define USE_JUDY_HASH
+//#define USE_HASH_MAP
+//#define EMPTY_LOOP
 
 #include "judyhash.h"
+
+#ifdef USE_HASH_MAP
+#include <hash_map>
+#endif
 
 struct hsh_string_hash {
 	unsigned long operator () (const char *key) const
 	{
+//		const int m = 7U;
+//		const int m = 31U;
+//		const int m = 33U;
+//		const int m = 65599U;
+		const int m = 2654435789U;
+
 		unsigned h = 0;
 
 		while (*key)
-			h = h * 7 + *key++;
+			h = h * m + *key++;
 
 		return h ; 
 	}
@@ -32,16 +45,18 @@ struct cmp_string_lt {
 	}
 };
 
-#if 1
-//typedef std::map <const char *, int, cmp_string_lt> my_hash;
-typedef std::map <std::string, int> my_hash;
-//typedef std::hash_map <
-//	const char *, int, hsh_string_hash, hsh_string_eq
-//	> my_hash;
-#else
+#ifdef USE_JUDY_HASH
 typedef judyhash_map <
 	const char *, int, hsh_string_hash, cmp_string_eq
 	> my_hash;
+#else
+#ifdef USE_HASH_MAP
+typedef std::hash_map <
+	const char *, int//, hsh_string_hash, cmp_string_eq
+	> my_hash;
+#else
+typedef std::map <std::string, int> my_hash;
+#endif
 #endif
 
 int main ()
@@ -50,6 +65,16 @@ int main ()
 	int line_count = 100;
 
 	int dups = 0;
+
+#ifdef USE_JUDY_HASH
+	std::cout << "JudyHash!!!\n";
+#else
+#ifdef USE_HASH_MAP
+	std::cout << "std::hash_map!!!\n";
+#else
+	std::cout << "std::map!!!\n";
+#endif
+#endif
 
 	my_hash::value_type init_values [] = {
 		my_hash::value_type ("record", 1000),
@@ -70,9 +95,12 @@ int main ()
 	while (fgets (line, sizeof (line), stdin)){
 //		std::pair <my_hash::key_type, my_hash::mapped_type> curr;
 		std::pair <my_hash::iterator, bool> curr
+#ifndef EMPTY_LOOP
 			= ht.insert (
 				my_hash::value_type (
-					strdup (line), line_count));
+					strdup (line), line_count))
+#endif
+			;
 
 		bool new_item = curr.second;
 		if (!new_item){
@@ -87,8 +115,14 @@ int main ()
 		++line_count;
 	}
 
+#ifdef USE_JUDY_HASH
 	std::cout << "duplicates count: " << dups << '\n';
 	std::cout << "map size: " << ht.size () << '\n';
+
+	std::cout << "single item count:" << ht.m_debug_info.m_value_count << '\n';
+	std::cout << "list item count:  " << ht.m_debug_info.m_list_item_count << '\n';
+	std::cout << "list count:       " << ht.m_debug_info.m_list_count << '\n';
+#endif
 
 //	my_hash::iterator beg, end;
 //	beg = ht.begin ();
