c1e354ebc91203f06cb88752622dc7fd628d53c2
Aleksey Cheusov <vle@gmx.net>
2005-07-29 12:08:05 +0000

    new tests

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ struct hsh_string_hash {
 		unsigned h = 0;
 
 		while (*key)
-			h = h * 33 + *key++;
+			h = h * 7 + *key++;
 
 		return h ; 
 	}
@@ -32,7 +32,7 @@ struct cmp_string_lt {
 	}
 };
 
-#if 0
+#if 1
 //typedef std::map <const char *, int, cmp_string_lt> my_hash;
 typedef std::map <std::string, int> my_hash;
 //typedef std::hash_map <
@@ -46,30 +46,56 @@ typedef judyhash_map <
 
 int main ()
 {
-	my_hash ht;
-
-	std::string line;
+	char line [2048];
 	int line_count = 100;
-	while (std::getline (std::cin, line)){
+
+	int dups = 0;
+
+	my_hash::value_type init_values [] = {
+		my_hash::value_type ("record", 1000),
+		my_hash::value_type ("access", 1000),
+		my_hash::value_type ("the", 1000),
+		my_hash::value_type ("27562356273562036503276502560265", 1000),
+		my_hash::value_type ("layout", 1000)
+	};
+
+	my_hash ht2 (
+		init_values,
+		init_values + sizeof (init_values)/sizeof (init_values [0]));
+
+//	my_hash ht = ht2;
+	my_hash ht;
+	ht.swap (ht2);
+
+	while (fgets (line, sizeof (line), stdin)){
 //		std::pair <my_hash::key_type, my_hash::mapped_type> curr;
-		my_hash::iterator curr
+		std::pair <my_hash::iterator, bool> curr
 			= ht.insert (
 				my_hash::value_type (
-					strdup (line.c_str ()), line_count));
+					strdup (line), line_count));
 
-		std::cout << "key=`" << (*curr).first << "`\n";
-		std::cout << "value=" << (*curr).second << '\n';
-		std::cout << '\n';
+		bool new_item = curr.second;
+		if (!new_item){
+			++dups;
+		}
+
+//		std::cout << "new=`" << curr.second << "`\n";
+//		std::cout << "key=`" << (*curr.first).first << "`\n";
+//		std::cout << "value=" << (*curr.first).second << '\n';
+//		std::cout << '\n';
 
 		++line_count;
 	}
 
-	my_hash::iterator beg, end;
-	beg = ht.begin ();
-	end = ht.end ();
+	std::cout << "duplicates count: " << dups << '\n';
+	std::cout << "map size: " << ht.size () << '\n';
 
-	for (; !(beg == end); ++beg){
-		std::cout << "key2=`" << (*beg).first << "`\n";
-		std::cout << "value2=" << (*beg).second << "\n";
-	}
+//	my_hash::iterator beg, end;
+//	beg = ht.begin ();
+//	end = ht.end ();
+
+//	for (; !(beg == end); ++beg){
+//		std::cout << "key2=`" << (*beg).first << "`\n";
+//		std::cout << "value2=" << (*beg).second << "\n";
+//	}
 }
